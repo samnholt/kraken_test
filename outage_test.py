0c2c053f45d2dfd1775764accf5ab1ea8421f2d2
Sam Holt <sam@carbon.coop>
2023-01-22 17:09:13 +0000

    Format outage_test with black

--- outage_test.py
+++ outage_test.py
@@ -2,73 +2,89 @@ import json
 import os
 import requests
 import datetime
-import pytz
 
 from dateutil import parser, tz
 from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
-API_URL = os.getenv('API_URL')
-SITE_ID = os.getenv('SITE_ID')
+API_KEY = os.getenv("API_KEY")
+API_URL = os.getenv("API_URL")
+SITE_ID = os.getenv("SITE_ID")
 
 selected_outages = []
 
+
 def return_json(URL):
+    """Ensure that program throws an exception in case of 500 status code"""
     response = requests.get(URL, headers={"x-api-key": API_KEY})
 
     if response.status_code != 500:
-      json_obj = response.json()
-      return json_obj
+        json_obj = response.json()
+        return json_obj
     else:
-        # Whoops it wasn't a 200
         raise Exception("Oops! Request returned a 500")
+
 
 # Fetch all outages
 def get_all_outages():
-  URL = f"{API_URL}outages"
-  outages = return_json(URL=URL)
-  return outages
+    URL = f"{API_URL}outages"
+    outages = return_json(URL=URL)
+    return outages
+
 
 # Fetch site info
 def get_site_info():
-  URL = f"{API_URL}site-info/{SITE_ID}"
-  site_info = return_json(URL=URL)
-  return site_info
+    URL = f"{API_URL}site-info/{SITE_ID}"
+    site_info = return_json(URL=URL)
+    return site_info
+
 
 # Process outages, removing those before the given date and outside the site
 def process_data(outages, site_info):
-  for outage in outages:
-      if outage['id'] in [device['id'] for device in site_info['devices']]:
-          if parser.parse(outage['begin']) >= datetime.datetime(2022, 1, 1, tzinfo=tz.tzutc()):
-            selected_outages.append(outage)
+    for outage in outages:
+        if outage["id"] in [device["id"] for device in site_info["devices"]]:
+            if parser.parse(outage["begin"]) >= datetime.datetime(
+                2022, 1, 1, tzinfo=tz.tzutc()
+            ):
+                selected_outages.append(outage)
 
-  for selected_outage in selected_outages:
-    id = selected_outage['id']
-    for device in site_info['devices']:
-      if device['id'] == id:
-        selected_outage['name'] = device['name']
+    for selected_outage in selected_outages:
+        id = selected_outage["id"]
+        for device in site_info["devices"]:
+            if device["id"] == id:
+                selected_outage["name"] = device["name"]
 
-  processed_outages = json.dumps(selected_outages)
+    processed_outages = json.dumps(selected_outages)
 
-  return processed_outages
+    return processed_outages
+
 
 # Send list of processed outages to post endpoint
 def post_outages(outages):
-  post_response = requests.post(f"{API_URL}site-outages/{SITE_ID}", headers={"x-api-key": API_KEY, "accept": "*/*", "Content-Type": "application/json"}, data=outages)
-  if post_response.status_code != 500:
-    result = post_response.status_code
-    return result
-  else:
-    raise Exception("Oops! Request returned a 500.")
+    post_response = requests.post(
+        f"{API_URL}site-outages/{SITE_ID}",
+        headers={
+            "x-api-key": API_KEY,
+            "accept": "*/*",
+            "Content-Type": "application/json",
+        },
+        data=outages,
+    )
+    if post_response.status_code != 500:
+        result = post_response.status_code
+        return result
+    else:
+        raise Exception("Oops! Request returned a 500.")
+
 
 def main():
-  outages = get_all_outages()
-  info = get_site_info()
-  processed = process_data(outages=outages, site_info=info)
-  result = post_outages(outages=processed)
-  print(result)
+    outages = get_all_outages()
+    info = get_site_info()
+    processed = process_data(outages=outages, site_info=info)
+    result = post_outages(outages=processed)
+    print(result)
+
 
 if __name__ == "__main__":
-  main()
+    main()
