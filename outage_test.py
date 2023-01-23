b81228c6f34c4ec245d5d4a4271dae4bfac55315
Sam Holt <sam@carbon.coop>
2023-01-23 10:49:16 +0000

    Add logs

--- outage_test.py
+++ outage_test.py
@@ -2,9 +2,11 @@ import json
 import os
 import requests
 import datetime
+import logging
 
 from dateutil import parser, tz
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -20,6 +22,7 @@ def return_json(URL: str):
     response = requests.get(URL, headers={"x-api-key": API_KEY})
 
     if response.status_code != 500:
+        logging.debug('Server is OK')
         json_obj = response.json()
         return json_obj
     else:
@@ -28,6 +31,7 @@ def return_json(URL: str):
 
 def get_all_outages():
     """Fetch all outages"""
+    logging.info("Fetching all of outages")
     URL = f"{API_URL}outages"
     outages = return_json(URL=URL)
     return outages
@@ -35,6 +39,7 @@ def get_all_outages():
 
 def get_site_info():
     """Fetch site info"""
+    logging.info("Fetching site info")
     URL = f"{API_URL}site-info/{SITE_ID}"
     site_info = return_json(URL=URL)
     return site_info
@@ -42,11 +47,13 @@ def get_site_info():
 
 def process_data(outages: list, site_info: dict):
     """Process outages, removing those before the given date and outside the site"""
+    logging.info("Processing outages")
     for outage in outages:
         if outage["id"] in [device["id"] for device in site_info["devices"]]:
             if parser.parse(outage["begin"]) >= datetime.datetime(
                 2022, 1, 1, tzinfo=tz.tzutc()
             ):
+                logging.info(f"Found outage matching conditions: {outage['id']}")
                 selected_outages.append(outage)
 
     for selected_outage in selected_outages:
@@ -55,6 +62,7 @@ def process_data(outages: list, site_info: dict):
             if device["id"] == id:
                 selected_outage["name"] = device["name"]
 
+    logging.info("All relevant outages processed")
     processed_outages = json.dumps(selected_outages)
 
     return processed_outages
@@ -62,6 +70,7 @@ def process_data(outages: list, site_info: dict):
 
 def post_outages(outages: str):
     """Send list of processed outages to post endpoint"""
+    logging.info("Posting outages back to endpoint")
     post_response = requests.post(
         f"{API_URL}site-outages/{SITE_ID}",
         headers={
@@ -79,11 +88,13 @@ def post_outages(outages: str):
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     outages = get_all_outages()
     info = get_site_info()
     processed = process_data(outages=outages, site_info=info)
     result = post_outages(outages=processed)
-    print(result)
+    if result == 200:
+        logging.info("Received 200 from POST endpoint - success")
 
 
 if __name__ == "__main__":
